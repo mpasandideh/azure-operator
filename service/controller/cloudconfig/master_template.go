1cd67b3a098d88d356610a3c2129ba786b6fe2d8
Whites11 <whites11@gmail.com>
2020-05-03 16:40:08 +0200

    Added creation of storageaccount ad file share

--- service/controller/cloudconfig/master_template.go
+++ service/controller/cloudconfig/master_template.go
@@ -194,8 +194,8 @@ func (me *masterExtension) Files() ([]k8scloudconfig.FileAsset, error) {
 			Permissions: FilePermission,
 		},
 		{
-			AssetContent: fmt.Sprintf("username=%s\npassword=%s\n", "8tnh2etcd", "qmSCaKuVetCEzNKSZUx76D6w6QuyOWqncx9M2E0Kvqeec5bqXNwervHgXa90mGFlnNtOyY/hJsmJG4kDhQf49A=="),
-			Path:         "/etc/smbcredentials/8tnh2etcd.cred",
+			AssetContent: fmt.Sprintf("username=%setcd\npassword=%s\n", me.customObject.Name, "qmSCaKuVetCEzNKSZUx76D6w6QuyOWqncx9M2E0Kvqeec5bqXNwervHgXa90mGFlnNtOyY/hJsmJG4kDhQf49A=="),
+			Path:         fmt.Sprintf("/etc/smbcredentials/%setcd.cred", me.customObject.Name),
 			Owner: k8scloudconfig.Owner{
 				Group: k8scloudconfig.Group{
 					Name: FileOwnerGroupName,
--- service/controller/templates/ignition/etcd_mount_unit.go
+++ service/controller/templates/ignition/etcd_mount_unit.go
@@ -5,10 +5,10 @@ Description=Mounts azure file storage to /var/lib/etcd using CIFS
 Before=etcd3.service
 
 [Mount]
-What=//8tnh2etcd.file.core.windows.net/etcd
+What=//{{.Cluster.ID}}etcd.file.core.windows.net/etcd
 Where=/var/lib/etcd
 Type=cifs
-Options=nofail,vers=3.0,credentials=/etc/smbcredentials/8tnh2etcd.cred,serverino
+Options=nofail,vers=3.0,credentials=/etc/smbcredentials/{{.Cluster.ID}}etcd.cred,serverino
 
 [Install]
 WantedBy=multi-user.target
